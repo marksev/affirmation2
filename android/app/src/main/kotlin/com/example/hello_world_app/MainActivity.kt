91cdd47a7fd6d632091d87fb801c485ba5e210e6
marksev <33703765+marksev@users.noreply.github.com>
2025-08-25 18:23:26 +0000

    Fix project structure for Flutter

--- android/app/src/main/kotlin/com/example/hello_world_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/hello_world_app/MainActivity.kt
@@ -1,21 +1,6 @@
 package com.example.hello_world_app
 
-import android.app.Activity
-import android.os.Bundle
-import android.widget.TextView
-import android.view.Gravity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : Activity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        
-        // Create a TextView to display "HELLO WORLD"
-        val textView = TextView(this)
-        textView.text = "HELLO WORLD"
-        textView.textSize = 32f
-        textView.gravity = Gravity.CENTER
-        
-        // Set the TextView as the content view
-        setContentView(textView)
-    }
+class MainActivity: FlutterActivity() {
 }
